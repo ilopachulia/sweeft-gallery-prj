e612d93a5d7a09adc2d49d974d50cbcb3fafcb35
ilia.pachulia <ilia.pachulia@quantori.com>
2024-02-29 19:24:19 +0400

    added fetching functionality

---
+++ src/api/fetchGallery.ts
@@ -0,0 +1,14 @@
+import { BASE_URL } from "../helpers/constants";
+
+export const fetchGallery = async () => {
+    try {
+        const response = await fetch(`${BASE_URL}/photos/?client_id=${import.meta.env.VITE_APP_API_KEY}`)
+        const data = await response.json()
+        console.log('images', data);
+        return data
+    } catch (error) { 
+        console.log('error', error);
+    }
+
+    return null;
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,43 @@
-function Home(){
-    return <div>Home Page</div>
+import { useEffect, useState } from "react";
+import { fetchGallery } from "../api/fetchGallery";
+
+function Home() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      const data = await fetchGallery();
+      setImages(data);
+    };
+    fetchImages();
+  }, [])
+
+  const onChangeHandler = async () => {
+    const data = await fetchGallery();
+    console.log(data);
+    setImages(data);
+  };
+
+  return (
+    <>
+      <input type="search" onChange={onChangeHandler} />
+      {images.map((image) => {
+        return (
+          <div
+            key={image.id}
+            style={{
+              width: "50px",
+              height: "auto",
+              display: "flex",
+              justifyContent: "space-between",
+            }}
+          >
+            <img src={image.urls.regular} alt={image.alt_description} />
+          </div>
+        );
+      })}
+    </>
+  );
 }
 
 export default Home;
