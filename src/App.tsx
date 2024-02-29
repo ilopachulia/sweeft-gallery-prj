c2ac28095ae53f924a071e51c1a813bf765c072e
ilia.pachulia <ilia.pachulia@quantori.com>
2024-02-29 20:22:25 +0400

    added layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import { Route, Routes } from "react-router-dom";
-import "./App.css";
 import Home from "./pages/Home";
 import History from "./pages/History";
+import Layout from "./components/Layout";
 
 function App() {
   return (
-    <>
+    <Layout>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="history" element={<History />} />
       </Routes>
-    </>
+    </Layout>
   );
 }
 
